f28748d8db7400973bb18199cf2b9bb8ef45d8bf
= <yselivanov857@gmail.com>
2025-12-10 22:30:46 +0300

    [create]: main vue component project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import PagesLayout from "@components/layouts/PagesLayout.vue";
+</script>
+
+<template>
+  <PagesLayout>
+    <router-view></router-view>
+  </PagesLayout>
+</template>
