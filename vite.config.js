8dd6c3622fc0292ebc8f04b896dcc276b15e9094
= <yselivanov857@gmail.com>
2025-12-10 22:30:28 +0300

    [create]: main vite config

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+      "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+      "@images": fileURLToPath(new URL("./src/assets/images", import.meta.url)),
+      "@icons": fileURLToPath(new URL("./src/assets/icons", import.meta.url)),
+    },
+  },
+});
