6256e052fd792abbc4ec88a6ab33eb0937962b02
= <yselivanov857@gmail.com>
2025-12-14 15:51:44 +0300

    [update]: create main layout

--- src/components/layouts/PagesLayout.vue
+++ src/components/layouts/PagesLayout.vue
@@ -4,7 +4,7 @@ import Header from "@components/header/Header.vue";
 
 <template>
   <Header />
-  <main class="container mx-auto">
+  <main class="mx-auto">
     <slot></slot>
   </main>
 </template>
