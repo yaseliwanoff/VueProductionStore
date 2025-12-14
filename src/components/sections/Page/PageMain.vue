9af5adfee1aaeac09381afaa6de354d52aee96ba
= <yselivanov857@gmail.com>
2025-12-14 16:40:33 +0300

    [create]: main component for page

---
+++ src/components/sections/Page/PageMain.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Sidebar from "./Sidebar.vue";
+import Items from "./Items.vue";
+</script>
+
+<template>
+  <section>
+    <div class="flex items-start">
+      <div class="h-screen w-1/3 sidebar__block">
+        <Sidebar />
+      </div>
+      <div class="w-full items__block">
+        <Items />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+.sidebar__block {
+  background-color: rgb(14, 14, 14);
+  padding: 10px 20px;
+}
+
+.items__block {
+  padding: 0px 20px;
+}
+</style>
